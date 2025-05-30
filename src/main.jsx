e8330e48a14bed081f1008681ead2b9d258f2816
cbradford179 <cody.bradford7@gmail.com>
2025-05-30 16:56:08 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,4 @@
-// 1) Import your CSS first
-import './index.css';
-
-+// 2) Bring in React & Auth0
-+import React from 'react';
-+import ReactDOM from 'react-dom/client';
-+import { Auth0Provider } from '@auth0/auth0-react';
-import App from './App';
-
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(
-+    // 3) Wrap the entire app in Auth0Provider
-+    <Auth0Provider
-+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-+      authorizationParams={{ redirect_uri: window.location.origin }}
-+    >
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-+    </Auth0Provider>
-  );
+// 2) Bring in React & Auth0
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
